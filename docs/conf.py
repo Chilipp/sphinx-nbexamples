2e0d5444377fc0fb34a8759b72785f5b1c268d22
Chilipp <p.sommer@posteo.de>
2016-10-25 12:44:55 +0200

    Use check_call when registering ipython kernel

--- docs/conf.py
+++ docs/conf.py
@@ -67,9 +67,9 @@ example_gallery_config = dict(
 if on_rtd:
     import subprocess as spr
     print('Registering kernel')
-    spr.call([sys.executable] +
-             ('-m ipykernel install --user --name python3 '
-              '--display-name python3').split())
+    spr.check_call([sys.executable] +
+                   ('-m ipykernel install --user --name python3 '
+                    '--display-name python3').split())
 
 
 # The encoding of source files.
