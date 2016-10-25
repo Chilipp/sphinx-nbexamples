afcb454185d4c1f96025c7520590ba46f0d44dc3
Chilipp <p.sommer@posteo.de>
2016-10-25 13:28:28 +0200

    Use explicit kernel name

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,8 @@ example_gallery_config = dict(
 if on_rtd:
     import subprocess as spr
     spr.call([sys.executable] +
-             ('-m ipykernel install --user').split())
+             ('-m ipykernel install --user --name python3 '
+              '--display-name python3').split())
 
 
 # The encoding of source files.
