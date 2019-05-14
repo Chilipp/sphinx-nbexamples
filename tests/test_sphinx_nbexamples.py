69e54fc2e3625f6568265b4be1350f92ed9fe90e
Chilipp <p.sommer@posteo.de>
2019-05-15 00:03:08 +0200

    added tests for README.md

--- tests/test_sphinx_nbexamples.py
+++ tests/test_sphinx_nbexamples.py
@@ -191,6 +191,15 @@ class TestGallery(BaseTest):
                       msg=('The wrong picture has been chosen for '
                            'example_mpl_test_figure_chosen.ipynb'))
 
+    def test_md_readme(self):
+        """Test the conversion of README.md"""
+        html_path = osp.join(self.out_dir, 'examples', 'sub', 'index.html')
+        self.assertTrue(osp.exists(html_path),
+                        msg=html_path + ' is missing!')
+        with open(html_path) as f:
+            html = f.read()
+        self.assertIn('a markdown link</a>', html)
+
 
 @unittest.skipIf(pathlib is None, 'The pathlib package is required!')
 class TestLinkGalleries(BaseTest):
